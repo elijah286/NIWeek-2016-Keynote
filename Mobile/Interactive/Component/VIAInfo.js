df31a476e327ca83be3a7e5f91de9bc9feed5094
Mark Black <mark.prestwick@gmail.com>
2016-07-21 10:57:06 -0500

    Initial commit of mobile WebVI portion of the demo
    This includes source and the build output from the component.

---
+++ Mobile/Interactive/Component/VIAInfo.js
@@ -0,0 +1,13 @@
+//  via info  
+NationalInstruments.HtmlVI.viName = '_%4Dobile%2Egvi';
+
+//inputs: 
+NationalInstruments.HtmlVI.inputDataItem = {};
+NationalInstruments.HtmlVI.inputDataItem['di_74']='Boolean';
+NationalInstruments.HtmlVI.inputDataItem['di_75']='Boolean';
+
+//outputs:
+NationalInstruments.HtmlVI.outputDataItem = {};
+NationalInstruments.HtmlVI.outputDataItem['di_73']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_71']='Boolean';
+NationalInstruments.HtmlVI.outputDataItem['di_72']='Boolean';
